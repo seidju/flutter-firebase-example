ccadb2619225f4d0692e3baf7ee7743613e98020
Pavel Shatalov <shatalovp@gmail.com>
2019-06-19 11:43:37 +0700

    Add call button

--- lib/src/ui/estate_info.dart
+++ lib/src/ui/estate_info.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:real_estate_app/src/models/estate_model.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class EstateInfoWidget extends StatelessWidget {
 
@@ -40,18 +41,34 @@ class EstateInfoWidget extends StatelessWidget {
                 margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
-                    Padding(padding: EdgeInsets.only(bottom: 8.0)),
-                    Text(
+                    Padding(
+                      padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                      child: Text(
                       '${model.address.district}, улица ${model.address.street}',
+                      ),
                     ),
-                    Padding(padding: EdgeInsets.only(bottom: 8.0)),
-
                     Text(
                       '${model.roomCount}-к квартира, цена ${model.price} рублей',
                       style: TextStyle(fontWeight: FontWeight.bold),
                     ),
+
+                    const SizedBox(height: 8.0),
+
+                    RaisedButton(
+                      onPressed: () => launch("tel://${model.phone}"),
+                      color: Colors.green,
+                      child: Text(
+                        model.phone,
+                        style: TextStyle(fontSize: 20.0, color: Colors.black),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32.0)
+                      ),
+                    ),
+
+
                     Padding(
                         padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
                         child:Text(model.description),
